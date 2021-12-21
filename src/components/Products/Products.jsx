55391ec189a2b385417f36b09d32bbbed419d4c7
Khalfan1998 <khalfan.ikns@gmail.com>
2021-12-21 16:16:12 +0300

    renaming fade
    renamed fade to alpha

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -17,7 +17,7 @@ const Products = () => {
         <main className={classes.content}>
             {/*adding height to push content below navbar*/}
             <div className ={classes.toolbar} />
-        <Grid container justify="center" spacing={4}>
+        <Grid container justifyContent="center" spacing={4}>
         {products.map((product) => (
             <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                 <Product product={product}/>
