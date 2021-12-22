879f5948860d1a83ed2425d28167e7710e5fcb86
Khalfan1998 <khalfan.ikns@gmail.com>
2021-12-22 11:42:41 +0300

    Added Cart and Buttons
    added shopping cart and the buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
-import { Products, Navbar } from "./components";
+import { Products, Navbar, Cart } from "./components";
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -18,6 +18,12 @@ const App = () => {
     /*get cart*/
     setCart(await commerce.cart.retrieve());
   };
+  /*add items to cart */
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
 
   useEffect(() => {
     fetchProducts();
@@ -28,8 +34,9 @@ const App = () => {
 
   return (
     <div>
-      <Navbar />
-      <Products products={products} />
+      <Navbar totalItems={cart.total_items} />
+      {/*<Products products={products} onAddToCart={handleAddToCart} /> */}
+      <Cart cart={cart} />
     </div>
   );
 };
