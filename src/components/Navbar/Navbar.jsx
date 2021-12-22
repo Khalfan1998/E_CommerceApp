37d9e841766d35827c5aa4b599cec4a473eb942b
Khalfan1998 <khalfan.ikns@gmail.com>
2021-12-22 12:32:17 +0300

    Cart Icon Update
    added logic to remove cart icon if on cart page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
-//import { mergeClasses } from '@material-ui/styles';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from '../../assets/logo.jpg'
 import useStyles from './styles';
 
 const Navbar = ({totalItems}) => {
     const classes = useStyles();
+    const location = useLocation();
+
+    
     
     return (
         <>
             <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
-                    <Typography variant='h6' className={classes.title} color='inherit'>
+                    <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
                         <img src={logo} alt='Date Shop' height='25px' className={classes.image}/>
                         Date Shop
                     </Typography>
                     <div className={classes.grow} />
+                    {location.pathname === '/' && (
                     <div className={classes.button}>
-                        <IconButton aria-label='Show cart items' color='inherit'>
+                        <IconButton component={Link} to='/cart' aria-label='Show cart items' color='inherit'>
                             <Badge badgeContent={totalItems} color='secondary'>
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div>
+                    </div>)}
                     
                 </Toolbar>
             </AppBar>
