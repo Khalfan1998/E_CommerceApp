861eadf6fc109b8b6e1cc227b887652aef90b875
Khalfan1998 <khalfan.ikns@gmail.com>
2021-12-26 12:24:39 +0300

    Revert "Revert "payment confirmation""
    This reverts commit 357909b02ef88253b232df451ae2d7b4d22a50c0.

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core';
-
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, CssBaseline} from '@material-ui/core';
+import {Link, useNavigate } from 'react-router-dom';
 import { commerce } from '../../../lib/commerce';
 import useStyles from './styles';
 import AddressForm from '../AddressForm';
@@ -12,7 +12,9 @@ const Checkout = ({cart, order, onCaptureCheckout, error}) => {
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckOutToken] = useState(null);
     const [shippingData, setShippingData] = useState({});
+    const [isFinished, setIsFinished] = useState(false);
     const classes = useStyles();
+    
 
     useEffect(() => {
         const generateToken = async () => {
@@ -24,7 +26,7 @@ const Checkout = ({cart, order, onCaptureCheckout, error}) => {
                 setCheckOutToken(token);
 
             } catch (error) {
-                
+               
             }
         }
 
@@ -42,19 +44,66 @@ const Checkout = ({cart, order, onCaptureCheckout, error}) => {
 
     }
 
-    const Confirmation = () => (
+    const timeout = () => {
+        setTimeout(() => {
+            setIsFinished(true)
+        }, 3000);
+
+    }
+
+    let Confirmation = () => order.customer ? (
+        <>
         <div>
-            Confirmation
+            <Typography variant='h5' >
+                Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}  </Typography>
+                <Divider className={classes.divider}/>
+                <Typography variant='subtitle2'>
+                    Order ref: {order.customer_reference}
+                </Typography>
+            
+        </div>
+        <br />
+        <Button component={Link} to='/' variant='outlined' type='button'>Back to Home</Button>
+        
+        </>
+    ) : isFinished ? (
+        <>
+        <div>
+            <Typography variant='h5' >
+                Thank you for your purchase </Typography>
+                <Divider className={classes.divider}/>
+                
+            
+        </div>
+        {/*
+        <br />
+        <Button component={Link} to='/' variant='outlined' type='button'>Back to Home</Button> */}
+        
+        </>
+    ) :(
+        <div className={classes.spinner}>
+            <CircularProgress />
         </div>
     );
 
+    /*if(error) {
+        <>
+            <Typography variant='h5'>
+                Error: {error}
+            </Typography>
+            <br />
+            <Button component={Link} to='/' variant='outlined' type='button'>Back to Home</Button>
+        </>
+    }*/
+
     const Form = () => activeStep === 0
         ? <AddressForm checkoutToken={checkoutToken} next ={next} />
-        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout}/>
+        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout} timeout={timeout}/>
 
            
     return (
         <>
+        <CssBaseline />
           <div className={classes.toolbar} /> 
           <main className={classes.layout}>
               <Paper className={classes.paper}>
--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -9,18 +9,21 @@ const { control } = useFormContext();
 
 return (
    <>
+   <Grid item xs={12} sm={6}>
          <Controller
             control={control}
             name={name}
             render = {({ field})=> (
                 <TextField
+                defaultValue=''
                     fullWidth
                     label={label}
                     required
                 />
             )}
-         />
+         /></Grid>
    </>
+   
  );
  }
 
