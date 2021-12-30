fee6bad956e60be04a2901e800423464be9037bf
Khalfan1998 <khalfan.ikns@gmail.com>
2021-12-30 16:26:17 +0300

    Text change
    made your shopping cart text in bold

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -38,7 +38,7 @@ const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
         
         <Container marginBottom>
             <div className={classes.toolbar}/>
-            <Typography className={classes.title} variant='h3' gutterBottom>Your Shopping Cart</Typography>
+            <Typography className={classes.title} variant='h3' gutterBottom><strong>Your Shopping Cart</strong></Typography>
             { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
         </Container>
         
